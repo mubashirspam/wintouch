a785f8a98bd48b6612e012d78fe83c604384d8ee
Mubashir Ahmed <mymac@Mubashirs-MacBook-Air.local>
2026-01-10 22:37:12 +0530

    cdn

--- app/components/ui/index.ts
+++ app/components/ui/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from "./Button";
 export { default as Card } from "./Card";
 export { default as SectionHeading } from "./SectionHeading";
 export { default as ScholarshipFormModal } from "./ScholarshipFormModal";
+export { default as ScholarshipPopup } from "./ScholarshipPopup";
